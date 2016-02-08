1f9d5f1041d36643810073d8352cce0f8feebc6d
Harshal Patel <harshalpatel@gmail.com>
2016-02-08 14:27:09 -0500

    Add Comment

--- ex6.py
+++ ex6.py
@@ -1,3 +1,4 @@
+#This is the first string within a string
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
@@ -6,14 +7,17 @@ y = "Those who know %s and those who %s." % (binary, do_not)
 print x
 print y
 
+#This is the second string within a string
 print "I said: '%s'." % y
 print "I also said: '%s." % y
 
+#This is the third string within a string
 hilarious = False
 joke_evaluation = "Itsn't that joke so funny?! %r"
 
 print joke_evaluation % hilarious
 
+#This is the fourth string within a string
 w = "This is the left side of..."
 e = "a string with a right side."
 
