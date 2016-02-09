abfa72bbae8be7ebb0c1c47fc86a8140120a22fb
Harshal Patel <harshalpatel@gmail.com>
2016-02-09 16:43:11 -0500

    Add Line

--- ex12.py
+++ ex12.py
@@ -4,3 +4,7 @@ weight = raw_input ("How much do you weigh? ")
 
 print "So, you're %r old, %r tall and %r heavy." % (
 	age, height, weight) 
+
+hobbies = raw_input("What do you like to do over the weekend? ")
+
+print "So you like %r but you will have to be working on the following instead:" % (hobbies)
