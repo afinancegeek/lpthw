ae804d5f17813c6c58f928ff80893d13b7092361
Harshal Patel <harshalpatel@gmail.com>
2016-02-09 16:41:42 -0500

    Add Again

--- ex13.py
+++ ex13.py
@@ -1,11 +1,19 @@
 # imports the system module
 from sys import argv
 
-script, first, second, third, fourth, fifth = argv
+script, age, height, weight, hair_color = argv
 
+# added several arguments
 print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is:", second
-print "Your third variable is:", third
-print "Your fourth variable is:", fourth
-print "Your fifth variable is:", fifth
+print "Your first variable is:", age
+print "Your fourth variable is:", height
+print "Your third variable is:", weight
+print "Your fourth variable is:", hair_color
+
+age = raw_input("How old are you?")
+height = raw_input("How tall are you? ")
+weight = raw_input ("How much do you weigh? ")
+hair_color = raw_input ("What color is your hair? ")
+
+print "So, you're %ryr's old, %rft tall, %rlbs heavy, and have %r hair." % (
+	age, height, weight, hair_color) 
