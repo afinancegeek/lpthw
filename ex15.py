efe1a0bd0100f5fe339701dd3df50a4d794e3536
Harshal Patel <harshalpatel@gmail.com>
2016-02-10 16:27:42 -0500

    Add Again

--- ex15.py
+++ ex15.py
@@ -1,17 +1,31 @@
+# imports argument variable from system variable
 from sys import argv
 
+# list of arguments including self
 script, filename = argv
 
-txt = open(ex15_sample.txt)
+""""
+sets txt to open file object ex15_sample.txt using the file type
+.txt
 
+"""
+txt = open(filename)
+
+# prints output filename using raw_input entered by user
 print "Here's your file %r:" % filename
 print txt.read()
 
-print "Type the filename again:"
-file_again = raw_input(">")
+"""
+ask user for raw_input again and re-prints the filename
+based on raw_input
 
-txt_again = open(file_again)
+"""
+print "Type the filename again:"
+file_again = raw_input ("> ")
+
+# reopens file object ex15_sample.txt and re-prints filename
 
 txt_again = open(file_again)
 
 print txt_again.read()
+
